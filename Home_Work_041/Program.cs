9f9bfef016b88eac776103a78de142ca73c86c54
oleg Zhollobov <jolobov777@mail.ru>
2023-06-07 20:40:09 +0500

    task number forty-one

---
+++ Home_Work_041/Program.cs
@@ -0,0 +1,21 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+Console.WriteLine("Введите числа: ");
+string numbers = Console.ReadLine()!;
+string [] array = numbers.Split(',');
+Console.WriteLine($"{Numofnum(array)}");
+
+
+int Numofnum (string[] array)
+{
+   int count = 0;
+   for(int i = 0; i < array.Length; i++)
+   {
+        if (Convert.ToInt32(array[i]) > 0)
+           count++;
+   }
+   return count;
+}   
