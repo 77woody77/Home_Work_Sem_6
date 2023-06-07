7352fd2271c0901c546cab17cb8955f9f0851c01
oleg Zhollobov <jolobov777@mail.ru>
2023-06-07 20:47:21 +0500

    task number forty-five

---
+++ Home_Work_045/Program.cs
@@ -0,0 +1,29 @@
+﻿// Не используя рекурсию, выведите первые N чисел Фибоначчи.
+// Первые два числа Фибоначчи: 0 и 1.
+
+// Если N = 5-> 0 1 1 2 3
+
+// Если N = 3 -> 0 1 1
+
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+Console.Clear();
+Console.WriteLine("Введите число операций");
+int N = int.Parse(Console.ReadLine());
+
+int a = 0;
+int b = 1;
+int c = 0;
+
+for (int i = 0; i < N - 2; i++)
+    {
+        c = a + b;
+        a = b; 
+        b = c;
+        {
+            Console.WriteLine(c);
+        }
+
+    } 
+
+
